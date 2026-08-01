57d1b8e38fcfe975c8e2adbcf617185ece2c0570
Alicia Zamorano <zamoranoalicia@gmail.com>
2026-08-01 18:42:01 -0400

    [IMS-010]: Delete src/main/resources/db/migration/V2__CREATE_PRODUCT_CATEGORY.sql

--- src/main/resources/db/migration/V2__CREATE_PRODUCT_CATEGORY.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE product_category
-(
-    categoryId  UUID NOT NULL,
-    name        VARCHAR(255),
-    description VARCHAR(255),
-    createdAt   TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updatedAt   TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT pk_product_category PRIMARY KEY (categoryId)
-);
